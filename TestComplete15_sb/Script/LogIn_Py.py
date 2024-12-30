ac70cdcf57218bf80c1b686e573c9af89e4b18ad
Daniel Lagomarsino <Daniel.Lagomarsino@smartbear.com>
2024-12-30 13:45:19 -0500

    Commit 12/20/24

---
+++ TestComplete15_sb/Script/LogIn_Py.py
@@ -0,0 +1,21 @@
+﻿def Login():
+    #Launches the specified browser and opens the specified URL in it.
+    Browsers.Item[btChrome].Run("https://bearstore-testsite.smartbear.com/")
+    #Maximizes the specified Window object.
+    Aliases.browser.BrowserWindow.Maximize()
+    #Clicks the 'linkLogIn' link.
+    Aliases.browser.pageBearstoreTestsiteSmartbearCo.header.navUsd.navMenubarMyAccount.linkLogIn.Click()
+    #Waits until the browser loads the page and is ready to accept user input.
+    Aliases.browser.pageShop.Wait()
+    #Clicks the 'textboxUsernameoremail' control.
+    Aliases.browser.pageShop.sectionContent.textboxUsernameoremail.Click()
+    #Sets the text 'djl' in the 'textboxUsernameoremail' text editor.
+    Aliases.browser.pageShop.sectionContent.textboxUsernameoremail.SetText("djl")
+    #Sets the text Project.Variables.Password1 in the 'passwordboxPassword' text editor.
+    Aliases.browser.pageShop.sectionContent.passwordboxPassword.SetText(Project.Variables.Password1)
+    #Clicks the 'buttonLogIn' button.
+    Aliases.browser.pageShop.sectionContent.buttonLogIn.ClickButton()
+    #Delays the test execution for the specified time period.
+    Delay(1000)
+    #Checks whether the 'contentText' property of the Aliases.browser.pageBearstoreTestsiteSmartbearCo.FindElement("//h1[.='Welcome to our store.']") object equals 'Welcome to our store.'.
+    aqObject.CheckProperty(Aliases.browser.pageBearstoreTestsiteSmartbearCo.FindElement("//h1[.='Welcome to our store.']"), "contentText", cmpEqual, "Welcome to our store.")
---
+++ TestComplete15_sb/Script/loop.py
@@ -0,0 +1,40 @@
+﻿def SmartBank_Checking_form():
+    #Launches the specified browser and opens the specified URL in it.
+    Browsers.Item[btChrome].Run("http://smartbank.se.smartbear.io/")
+    #Maximizes the specified Window object.
+    Aliases.browser.BrowserWindow.Maximize()
+    Project.Variables.Varloop.Reset()
+    RecordIdx = 1
+    while RecordIdx <= 10:
+        #Clicks the 'textnode' control.
+        Aliases.browser.pageHome.header.nav.textnodePersonal.textnode.Click()
+        #Clicks the 'linkChecking' link.
+        Aliases.browser.pageHome.header.nav.textnodeChecking.linkChecking.Click()
+        #Waits until the browser loads the page and is ready to accept user input.
+        Aliases.browser.pageHome2.Wait()
+        #Clicks the 'textboxFirstName' control.
+        Aliases.browser.pageHome2.textnodePersonalInformation.textboxFirstName.Click()
+        #Sets the text KeywordTests.SmartBank_Checking_form.Variables.Varloop["FirstName"] in the 'textboxFirstName' text editor.
+        Aliases.browser.pageHome2.textnodePersonalInformation.textboxFirstName.SetText(Project.Variables.Varloop.Value["FirstName"])
+        #Sets the text KeywordTests.SmartBank_Checking_form.Variables.Varloop["LastName"] in the 'textboxLastName' text editor.
+        Aliases.browser.pageHome2.textnodePersonalInformation.textboxLastName.SetText(Project.Variables.Varloop.Value["LastName"])
+        #Enters KeywordTests.SmartBank_Checking_form.Variables.Varloop["DOB"] in the 'dateinputDateOfBirth' object.
+        Aliases.browser.pageHome2.textnodePersonalInformation.dateinputDateOfBirth.Keys(Project.Variables.Varloop.Value["DOB"])
+        #Sets the text KeywordTests.SmartBank_Checking_form.Variables.Varloop["Address"] in the 'textboxAddress1' text editor.
+        Aliases.browser.pageHome2.textnodePersonalInformation.textboxAddress1.SetText(Project.Variables.Varloop.Value["Address"])
+        #Sets the text KeywordTests.SmartBank_Checking_form.Variables.Varloop["City"] in the 'textboxCity' text editor.
+        Aliases.browser.pageHome2.textnodePersonalInformation.textboxCity.SetText(Project.Variables.Varloop.Value["City"])
+        #Sets the text KeywordTests.SmartBank_Checking_form.Variables.Varloop["State"] in the 'textboxState' text editor.
+        Aliases.browser.pageHome2.textnodePersonalInformation.textboxState.SetText(Project.Variables.Varloop.Value["State"])
+        #Sets the text KeywordTests.SmartBank_Checking_form.Variables.Varloop["Zip"] in the 'textboxZipCode' text editor.
+        Aliases.browser.pageHome2.textnodePersonalInformation.textboxZipCode.SetText(Project.Variables.Varloop.Value["Zip"])
+        #Sets the text KeywordTests.SmartBank_Checking_form.Variables.Varloop["Email"] in the 'emailinputEmailAddress' text editor.
+        Aliases.browser.pageHome2.textnodePersonalInformation.emailinputEmailAddress.SetText(Project.Variables.Varloop.Value["Email"])
+        #Sets the text KeywordTests.SmartBank_Checking_form.Variables.Varloop["Phone"] in the 'phoneinputPhoneNumber' text editor.
+        Aliases.browser.pageHome2.textnodePersonalInformation.phoneinputPhoneNumber.SetText(Project.Variables.Varloop.Value["Phone"])
+        #Sets the text KeywordTests.SmartBank_Checking_form.Variables.Varloop["Occupation"] in the 'phoneinputOccuptation' text editor.
+        Aliases.browser.pageHome2.textnodePersonalInformation.phoneinputOccuptation.SetText(Project.Variables.Varloop.Value["Occupation"])
+        #Clicks the 'buttonSubmit' button.
+        Aliases.browser.pageHome2.textnodePersonalInformation.buttonSubmit.ClickButton()
+        Project.Variables.Varloop.Next()
+        RecordIdx = RecordIdx + 1
